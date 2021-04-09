8b9901846697ce7e47078ec1b66dfefe2d98e4b3
Benton Li <cl2597@cornell.edu>
2021-04-10 03:48:18 +0800

    Add slide animation

--- lesson-4-homework/src/App.js
+++ lesson-4-homework/src/App.js
@@ -6,23 +6,44 @@ import c4 from './images/cornell4.jpeg';
 import c5 from './images/cornell5.jpeg';
 import c6 from './images/cornell6.jpeg';
 import c7 from './images/cornell7.jpeg';
-
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import reactDom from 'react-dom';
+import React, { useState } from 'react';
+import {useTransition, animated} from 'react-spring'
 
-function Buttons(){
+//气死我了，react-spring demo用的是8.0.5，我用9.10.0怎么也写不出来
+
+function App(){
   const images = [c0, c1, c2, c3, c4, c5, c6, c7]
-  const [imgIdx, setImageIdx] = useState(0);
-  return (
-    <>
-      <button onClick={() => setImageIdx(imgIdx => ((imgIdx - 1 + 8) % 8))}> &#10094;{imgIdx}</button>,
-      <button onClick={() => setImageIdx(imgIdx => ((imgIdx + 1) % 8))}> &#10095; {imgIdx}</button>,
-      <div> <img src={images[imgIdx]} /> </div>
+  const [image, setImageIdx] = useState({imgIdx: 2, forward: true });
+  const props = [{
+    leave: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    from: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  },{
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  }]
+  const transitions = useTransition(image["imgIdx"], props[(1 * image['forward'])]);
+  return (<>
+    <container>
+      <button onClick={() => setImageIdx(image => ({
+        imgIdx: (image["imgIdx"] - 1 + 8) % 8, forward: false
+        }))}> &#10094; </button>
+      <button onClick={() => setImageIdx(image => ({
+        imgIdx: (image["imgIdx"] + 1) % 8, forward: true
+        }))}> &#10095; </button>
+      <div>{
+        transitions((style, item) => {
+          return <animated.div  style={style}> <img src={images[item]}/> 
+          </animated.div>
+        })
+      }
+      </div>
+    </container>
     </>
-
   );
 }
 
 
-export default Buttons;
+export default App;
